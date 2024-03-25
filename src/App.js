833b111ef4067ad4570213444f2628b20911d07e
Lyle Simmons <lylesimmons125@gmail.com>
2024-03-24 23:21:50 -0400

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+  <div>
+    <h1>Hi, I'm Lyle</h1>
+    <h2>About Me</h2>
+    <p>Biography</p>
+    <a href="https://github.com/lsimmo25">GitHub</a>
+    <a href="https://www.linkedin.com/in/lyle-simmons/">LinkedIn</a>
+  </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,42 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+
+test("displays a top-level heading with the text `Hi, I'm _____`", () => {
+    render(<App />)
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1
+    })
+
+    expect(topLevelHeading).toBeInTheDocument()
+})
+
+test("A second-level heading with the text `About Me`", () => {
+    render(<App />)
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /About Me/,
+        exact: true,
+        level: 2
+    })
+    expect(secondLevelHeading).toBeInTheDocument()
+})
+
+test("A paragraph for your biographing", () => {
+    render(<App />)
+    const biography = screen.getByText(/biography/i)
+    expect(biography).toBeInTheDocument()
+})
+
+test("Two links to Github and LinkedIn", () => {
+    render(<App />)
+
+    const githubLink = screen.getByRole("link", {name: /github/i})
+    expect(githubLink).toBeInTheDocument()
+    expect(githubLink).toHaveAttribute('href', "https://github.com/lsimmo25")
+
+    const linkedIn = screen.getByRole("link", {name: /linkedIn/i})
+    expect(linkedIn).toBeInTheDocument()
+    expect(linkedIn).toHaveAttribute('href', "https://www.linkedin.com/in/lyle-simmons/")
+})
